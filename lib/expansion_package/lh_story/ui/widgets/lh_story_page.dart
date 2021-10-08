e2ceaab50e8b0a53d14259e7c6614a0642400341
unknown <nguyentiendat130397mobi@gmail.com>
2021-10-08 17:27:05 +0700

    add test for story view

---
+++ lib/expansion_package/lh_story/ui/widgets/lh_story_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LhStoryPage extends StatefulWidget {
+  const LhStoryPage({ Key? key }) : super(key: key);
+
+  @override
+  _LhStoryPageState createState() => _LhStoryPageState();
+}
+
+class _LhStoryPageState extends State<LhStoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
