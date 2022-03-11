e25579a8b2b578a236a507f2205595331e455ae7
nam20156097 <20156097@student.hust.edu.vn>
2022-03-11 11:24:12 +0700

    fix size text

--- lib/ui/widgets/lhbase_app.dart
+++ lib/ui/widgets/lhbase_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:lhbase_v1/lhbase.dart';
 
@@ -25,22 +26,31 @@ abstract class LhBaseApp extends StatefulWidget {
 class _LhBaseAppState extends State<LhBaseApp> {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      // theme: ThemeData(fontFamily: 'OpenSans'),
+    return ScreenUtilInit(
+        designSize: Size(414, 890),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: () {
+          ScreenUtil.setContext(context);
+          return MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+              child: GetMaterialApp(
+                debugShowCheckedModeBanner: false,
+                // theme: ThemeData(fontFamily: 'OpenSans'),
 
-      translations: widget.onConfig().translations,
-      locale: widget.onConfig().locale,
-      supportedLocales: widget.onConfig().supportedLocales ?? [],
-      initialRoute: widget.onConfig().initialRoute,
-      getPages: widget.onConfig().getPages,
-      initialBinding: widget.onConfig().initialBinding,
-      home: widget.onConfig().home,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-    );
+                translations: widget.onConfig().translations,
+                locale: widget.onConfig().locale,
+                supportedLocales: widget.onConfig().supportedLocales ?? [],
+                initialRoute: widget.onConfig().initialRoute,
+                getPages: widget.onConfig().getPages,
+                initialBinding: widget.onConfig().initialBinding,
+                home: widget.onConfig().home,
+                localizationsDelegates: const [
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                ],
+              ));
+        });
   }
 }
